b6162d6f8ebc776d94416ffeb29e8414ebb98d3d
David Lampe <lampe@plan-p.de>
2018-05-24 17:29:31 +0200

    Version change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Datenschutz Hinweis
 Description: Besucher über Datenschutz informieren. ACF und Github Updater erforderlich.
-Version: 1.5.2
+Version: 1.6
 Author URI: http://plan-p.de
 GitHub Plugin URI: planpgmbh/datenschutz-hinweis
 */
